4f53c6e3c8b232bf176a8639b4c2f65c8e89b984
lukasknaf <lknaf@logitech.com>
2025-05-07 14:33:05 +0100

    Remove references

--- src/adobeSample/quickActions.js
+++ src/adobeSample/quickActions.js
@@ -64,7 +64,7 @@ import {getModule, getQuickActions} from '../services/ccEverywhere.js';
     /* base64Asset: base64 representation we pass into QA function */
     try {
     	var base64Asset = await window.electronAPI.retrieveImage()
-	console.log('Image: ', base64Asset.substring(0,18))
+	console.log('Image: ', base64Asset.substring(0,30))
     } catch (error) {
 	console.log('No image supplied')
 	docConfig = null
