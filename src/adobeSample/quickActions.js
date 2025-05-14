93bc1e132774844b5c6be9603d33882326991a98
lukasknaf <lknaf@logitech.com>
2025-05-14 11:39:41 +0100

    Make async

--- src/adobeSample/quickActions.js
+++ src/adobeSample/quickActions.js
@@ -63,7 +63,7 @@ import {getModule, getQuickActions} from '../services/ccEverywhere.js';
 
     /* base64Asset: base64 representation we pass into QA function */
     try {
-    	var base64Asset = window.electronAPI.retrieveImage()
+    	var base64Asset = await window.electronAPI.retrieveImage()
 	    console.log('Image: ', base64Asset)
     } catch (error) {
         console.log('No image supplied')
