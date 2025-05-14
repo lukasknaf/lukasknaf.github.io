05bf2475bdcbd0431ba1aa26e1d66dc34745b5e9
lukasknaf <lknaf@logitech.com>
2025-05-14 10:46:09 +0100

    Try synchronous image retrieval

--- src/adobeSample/quickActions.js
+++ src/adobeSample/quickActions.js
@@ -63,7 +63,7 @@ import {getModule, getQuickActions} from '../services/ccEverywhere.js';
 
     /* base64Asset: base64 representation we pass into QA function */
     try {
-    	var base64Asset = await window.electronAPI.retrieveImage()
+    	var base64Asset = window.electronAPI.retrieveImage()
 	    console.log('Image: ', base64Asset.substring(0,30))
     } catch (error) {
         console.log('No image supplied')
